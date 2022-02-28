d89c513e517f0c8bad9381792129e9eaa0ccc0c7
chuhx <chuhx1024@126.com>
2022-02-28 18:15:03 +0800

    feat: vuex use ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { createStore } from 'vuex'
+
+export interface State {
+    count: number
+}
 // 创建一个新的 store 实例
-const store = createStore({
+const store = createStore<State>({
     state () {
         return {
             count: 100,
