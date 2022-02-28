764cd9f60880ef8becef295b380d85129e84db02
chuhx <chuhx1024@126.com>
2022-02-28 15:44:27 +0800

    feat: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    'vue/setup-compiler-macros': true
   },
   extends: [
     'plugin:vue/essential',
@@ -17,5 +18,13 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 1 : 0,
+    indent: [2, 4], // js 4个空格缩进
+    'vue/html-indent': [2, 4], // html 4个空格缩进
+    eqeqeq: 0, // 允许使用 == !=
+    'comma-dangle': [2, 'always-multiline'], // 对象 数据 分行显示的 结尾必须加 ','
+    'quote-props': 0,
+    'vue/no-multiple-template-root': 0,
   }
 }
