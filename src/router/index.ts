e650fb51925293c1187ecd5074db2f8c9de3ebd2
chuhx <chuhx1024@126.com>
2022-02-28 17:57:22 +0800

    feat: vue-router use TS

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/home',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "home" */ '../views/home/index.vue'),
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import(/* webpackChunkName: "login" */ '../views/login/index.vue'),
+    },
+]
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        {
-            path: '/home',
-            name: 'Home',
-            component: () => import('../views/home/index.vue'),
-        },
-        {
-            path: '/login',
-            name: 'Login',
-            component: () => import('../views/login/index.vue'),
-        },
-    ],
+    routes,
 })
 
 export default router
