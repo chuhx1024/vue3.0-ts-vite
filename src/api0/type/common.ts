02d49a256df19b3f81880a89976a8f88ef2a4a1d
chuhx <chuhx1024@126.com>
2022-03-01 19:42:14 +0800

    feat: axios optimize

---
+++ src/api0/type/common.ts
@@ -0,0 +1,6 @@
+// 使用泛型 提取公共格式的接口定义
+export interface ILoginInfo {
+    logo_square: string,
+    logo_logo: string,
+    slide: string[],
+}
---
+++ src/utils/request0.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig } from 'axios'
+const service = axios.create({
+    baseURL: 'https://shop.fed.lagou.com/api/admin',
+})
+
+// request interceptor
+service.interceptors.request.use(
+    config => {
+        // config.headers['SDN-TOKEN'] = getToken() || ''
+
+        return config
+    },
+    error => {
+        console.log(error) // for debug
+        return Promise.reject(error)
+    },
+)
+// response interceptor
+service.interceptors.response.use(
+    response => {
+        const res = response.data || { code: 500, data: {}, message: '数据获取失败' }
+        return res
+    },
+    error => {
+        // return Promise.reject(error)
+        return error
+    },
+)
+
+export default <T = any>(config: AxiosRequestConfig) => {
+    return service(config).then(res => {
+        return res.data.data as T
+    })
+}
